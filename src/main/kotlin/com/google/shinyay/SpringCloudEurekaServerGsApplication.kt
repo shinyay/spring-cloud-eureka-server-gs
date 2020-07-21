a8c35e70a5f423c855a0f891d5d0a8c15e3cec63
shinyay <shinya.com@gmail.com>
2020-07-21 23:01:06 +0900

    Add annotation to enable Eureka Server

--- src/main/kotlin/com/google/shinyay/SpringCloudEurekaServerGsApplication.kt
+++ src/main/kotlin/com/google/shinyay/SpringCloudEurekaServerGsApplication.kt
@@ -2,8 +2,10 @@ package com.google.shinyay
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
 @SpringBootApplication
+@EnableEurekaServer
 class SpringCloudEurekaServerGsApplication
 
 fun main(args: Array<String>) {
